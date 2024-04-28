105af74a7f620a088485deeb7cd89f758ac7e7ae
github_Rohit73108 <rohitrana73108@gmail.com>
2024-04-28 23:09:09 +0530

    backend setup for the further work

---
+++ script.js
@@ -0,0 +1,27 @@
+const express= require('express')
+const app= express()
+// *********** routing **************
+
+app.use((req, res , next)=>{
+    console.log('middleware is running ');
+    next();
+}); 
+// ************* middlewares ********** 
+
+app.use((req, res,next)=>{
+    console.log("middleware is running for 2nd");
+    next(); 
+})
+// ************* middlewares ********** 
+
+
+app.get('/', function(req, res){
+   res.send("Hello Rohit ")
+ });
+
+app.get('/profile', (req, res)=>{
+    res.send("this is blank profile page")
+});
+
+app.listen(3000);
+
